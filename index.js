cab4ef55a3eba8f251341b5b6a7298dd866a44ea
Edgar <franky9202@gmail.com>
2022-05-08 01:28:19 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+
+function submitData(name, email){
+  const formData = {
+    name : "Steve", 
+    email: "steve@steve.com"
+  }
+  
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
+  })
+      .then(response => response.json())
+      .then(object => {
+        document.body.append(object.id)
+        console.log(object.id)
+      })
+      .catch(error => {
+        alert(error.message)
+        document.body.append(error.message)
+
+      })
+  
+}
+
